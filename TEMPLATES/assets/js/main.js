01c2e11af027ba950812b9ea342dfde81c78e5d3
phillipmugisa <phillipmugisa4@gmail.com>
2022-07-25 08:08:55 +0300

    added other pages

--- TEMPLATES/assets/js/main.js
+++ TEMPLATES/assets/js/main.js
@@ -1,3 +1,24 @@
+// show mobile menu
+const mobileMenu = document.querySelector('.md-tabs');
+const mobileMenuActivator = document.querySelector('button#md-tabs-activator');
+
+mobileMenuActivator.addEventListener('click', () => {
+    mobileMenu.classList.toggle('open');
+
+    console.log(mobileMenuActivator.querySelector('i.fa'))
+
+    if (mobileMenuActivator.querySelector('i.fa').classList.contains(fa-bars))
+    {
+        mobileMenuActivator.querySelector('i.fa').classList.remove('fa-bars');
+        mobileMenuActivator.querySelector('i.fa').classList.add('fa-close');
+    }
+    else
+    {
+        mobileMenuActivator.querySelector('i.fa').classList.add('fa-bars');
+        mobileMenuActivator.querySelector('i.fa').classList.remove('fa-close');
+    }
+})
+
 // hero section img changer
 const heroSection = document.querySelector('.hero');
 const heroSideImgs = document.querySelectorAll('.side-imgs img');
@@ -45,4 +66,63 @@ const setHeroBackground = () => {
     // autoImgChange();
 }
 
-setHeroBackground();
+// check if we are on homepage
+if (document.querySelector('.hero'))
+{
+    setHeroBackground();
+}
+
+
+// destination detail
+const destinationImgs = document.querySelectorAll('.destination-imgs img')
+let detailImgIndex = 0;
+
+const imgControls = document.querySelectorAll('.destination-imgs button');
+const destinationImgSource = document.querySelector('#img-source')
+
+    imgControls.forEach(cta => {
+        cta.addEventListener('click', () => {
+            if (cta.dataset.id === 'next')
+            {
+                if (detailImgIndex < destinationImgs.length - 1)
+                {
+                    detailImgIndex++;
+                }
+                else
+                {
+                    detailImgIndex = 0;
+                }
+            }
+            else
+            {
+                if (detailImgIndex > 0)
+                {
+                    detailImgIndex--;
+                }
+                else
+                {
+                    detailImgIndex = destinationImgs.length - 1;
+                }
+            }
+            console.log(detailImgIndex)
+            setDetailImg();
+        })
+    })
+const setDetailImg = () => {
+    destinationImgs.forEach(img => {
+        if (img.classList.contains('in-view'))
+        {
+            img.classList.remove('in-view');
+        }
+    })
+    destinationImgs[detailImgIndex].classList.add('in-view');
+
+    destinationImgSource.querySelector('#source').textContent = destinationImgs[detailImgIndex].dataset.source;
+
+    destinationImgSource.href = destinationImgs[detailImgIndex].dataset.source;
+}
+
+if (window.location.href.includes('destination'))
+{
+    setDetailImg();
+}
