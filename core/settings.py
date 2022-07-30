53b80356d9295452621f5be0fd239f1a78d02e6f
phillipmugisa <phillipmugisa4@gmail.com>
2022-07-30 08:17:15 +0300

    app ready for hosting

--- core/settings.py
+++ core/settings.py
@@ -156,8 +156,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # mailling 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'phillipmugisa4@gmail.com'
-EMAIL_HOST_PASSWORD = 'bvhoquoxjwclwfqq'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'phillipmugisa4@gmail.com'
