74af3a10ae37a6ce6c02b649909d25d50bf62516
phillipmugisa <phillipmugisa4@gmail.com>
2022-08-08 07:02:21 +0300

    refracting pagination in views

--- eruna/views.py
+++ eruna/views.py
@@ -56,10 +56,10 @@ class DestinationListView(View):
 
         paginator = Paginator(self.queryset.objects.all().order_by('-id'), PER_PAGE_COUNT)
 
+        page_num = request.GET.get('page', 1)
         if self.queryset.objects.all().count() > 0:
 
             # get current page number
-            page_num = request.GET.get('page', 1)
             destinations = paginator.page(page_num).object_list
 
             destination_cover_imgs = models.DestinationImage.objects.filter(
@@ -113,10 +113,11 @@ class CarListView(View):
         page_num = request.GET.get('page', 1)
         cars = paginator.page(page_num).object_list
 
-        car_cover_imgs = models.CarImage.objects.filter(
-            Q(car__id__gte = cars[len(cars) - 1].id),
-            Q(car__id__lte = cars[0].id)
-        )
+        if cars.count > 0:
+            car_cover_imgs = models.CarImage.objects.filter(
+                Q(car__id__gte = cars[len(cars) - 1].id),
+                Q(car__id__lte = cars[0].id)
+            )
 
         content_data = {
             'page_obj': paginator.page(page_num),
